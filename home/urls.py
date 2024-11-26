300c364a30ff5fc9162b2949e2f57eccb8a5d4ce
jesscarlett <canrrytan@hotmail.com>
2024-11-26 21:09:19 +1100

    8 commit site map

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
 
+sitemaps = {
+    'static': StaticViewSitemap
+}
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
